4ec97f38c47bf15abea99c812a3f225eafff1a6c
Felix Exner <exner@fzi.de>
2023-10-11 06:54:39 +0000

    Migrated ibl config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -177,9 +177,9 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = { char = '┊'},
     },
   },
 
