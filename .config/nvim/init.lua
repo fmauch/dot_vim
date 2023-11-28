f023f682a9e6df15450778e6b7899382f35ff2de
Felix Exner <exner@fzi.de>
2023-11-28 23:23:41 +0000

    Updated default language servers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -463,12 +463,8 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  clangd = {},
+  jedi_language_server = {},
 
   lua_ls = {
     Lua = {
