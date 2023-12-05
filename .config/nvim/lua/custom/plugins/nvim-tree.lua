e0866a4f8f5991071e88508e9008c1a9f05b7ff2
Felix Exner <exner@fzi.de>
2023-12-05 23:06:48 +0000

    nvim-tree change cwd on focus change

--- .config/nvim/lua/custom/plugins/nvim-tree.lua
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -1,12 +1,18 @@
 return {
- "nvim-tree/nvim-tree.lua",
+  "nvim-tree/nvim-tree.lua",
   version = "*",
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      update_focused_file = {
+        enable      = true,
+        update_root  = true,
+        ignore_list = {}
+      },
+    }
   end,
 
   vim.keymap.set({ 'n' }, '<leader>t', ':NvimTreeFindFileToggle<cr>', { silent = true })
