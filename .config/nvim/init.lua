7d29f121dfc1d8671abf97bb4ad2ae7cd8191600
Felix Exner <git@fexner.de>
2024-01-21 12:37:27 +0100

    Added some navigation keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -292,6 +292,10 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+vim.keymap.set({ 'n', 'v' }, '\\', ':nohl<CR>', { silent = true })
+vim.keymap.set('i', '<C-e>', '<ESC>A', { silent = true })
+vim.keymap.set('i', '<C-a>', '<ESC>I', { silent = true })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
