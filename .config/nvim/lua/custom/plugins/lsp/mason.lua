54ae72f7df3e32758c9d98cd6b4c890e8a1c6b3d
Felix Exner <git@fexner.de>
2024-04-10 23:59:52 +0200

    Migrate to each plugin to its own plugin file

---
+++ .config/nvim/lua/custom/plugins/lsp/mason.lua
@@ -0,0 +1,44 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
+
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
+
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        "clangd",
+        "jedi_language_server",
+        "lua_ls",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "black", -- python formatter
+        "pylint",
+        "vale",
+      },
+    })
+  end,
+}
---
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = false,
+      theme = 'onedark',
+      component_separators = '|',
+      section_separators = '',
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename', 'nvim_treesitter#statusline' },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
+    }
+  },
+}
