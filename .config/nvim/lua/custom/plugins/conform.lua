57951a6a0912a6b20afb31dca35c01d18870e36c
Felix Exner <exner@fzi.de>
2024-05-13 20:34:45 +0200

    Add conform for formatting

---
+++ .config/nvim/lua/custom/plugins/conform.lua
@@ -0,0 +1,17 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {},
+  config = function()
+    require("conform").setup({
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" },
+      -- Use a sub-list to run only the first available formatter
+      javascript = { { "prettierd", "prettier" } },
+    },
+
+    vim.keymap.set({"n", "v"}, "<leader>C", require("conform").format)
+  })
+  end
+}
