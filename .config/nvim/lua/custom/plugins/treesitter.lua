ad8101ad89cfec52b8df2040332ddbf94eaf2a48
Felix Exner <exner@fzi.de>
2024-10-18 22:25:00 +0200

    Disable swap shortcuts
    They were interfering with my switch header/cpp shortcut

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -61,15 +61,15 @@ return {
             ['[]'] = '@class.outer',
           },
         },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
+        --swap = {
+          --enable = true,
+          --swap_next = {
+            --['<leader>a'] = '@parameter.inner',
+          --},
+          --swap_previous = {
+            --['<leader>A'] = '@parameter.inner',
+          --},
+        --},
       },
     }
   end
