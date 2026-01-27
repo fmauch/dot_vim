90727a4e2d420228712f47b128b8d175eced5eaa
Felix Exner <git@fexner.de>
2026-01-27 16:41:26 +0100

    Add doxygen previewer

---
+++ .config/nvim/lua/custom/plugins/doxygen-previewer.lua
@@ -0,0 +1,12 @@
+return {
+  "hat0uma/doxygen-previewer.nvim",
+  opts = {},
+  dependencies = { "hat0uma/prelive.nvim" },
+  cmd = {
+    "DoxygenOpen",
+    "DoxygenUpdate",
+    "DoxygenStop",
+    "DoxygenLog",
+    "DoxygenTempDoxyfileOpen"
+  },
+}
