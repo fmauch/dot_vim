e844be27ed6e24e6b4e5f465f0a610eb9a3f0d24
Felix Exner <git@fexner.de>
2026-01-27 16:42:51 +0100

    Delete <C-e> mapping for nvim-tree

--- .config/nvim/lua/custom/plugins/nvim-tree.lua
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -6,6 +6,25 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    local function my_on_attach(bufnr)
+      local api = require("nvim-tree.api")
+
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      ---
+      -- OR use all default mappings
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- remove a default
+      vim.keymap.del("n", "<C-e>", { buffer = bufnr })
+
+      -- add your mappings
+      vim.keymap.set("n", "?",     api.tree.toggle_help,                  opts("Help"))
+      ---
+    end
+
     require("nvim-tree").setup {
       update_focused_file = {
         enable      = true,
@@ -14,6 +33,7 @@ return {
           ignore_list = {}
         },
       },
+      on_attach = my_on_attach,
       sync_root_with_cwd = false,
     }
   end,
