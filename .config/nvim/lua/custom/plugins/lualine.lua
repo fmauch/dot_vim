117ff41cc27d90edc9acad427fe2da8214a33ed7
Felix Exner <git@fexner.de>
2026-05-04 10:09:13 +0200

    Fixes

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     options = {
       icons_enabled = false,
-      theme = 'onedark',
+      theme = 'auto',
       component_separators = '|',
       section_separators = '',
     },
